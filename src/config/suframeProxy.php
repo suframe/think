80b88ea5d7c449d8c0de008f0fa90696706ba6d5
shulinqian <330576744@qq.com>
2020-01-04 16:14:34 +0800

    配置增加env

--- src/config/suframeProxy.php
+++ src/config/suframeProxy.php
@@ -4,9 +4,9 @@ return [
     'driver' => 'suframe',
     'path' => '/user',
     'name' => 'user', //用于接口生成命名空间
-    'host' => '127.0.0.1',
-    'port' => '8081',
-    'rpcPort' => '9009',
+    'host' => env('suframeProxy.host', '127.0.0.1'),
+    'port' => env('suframeProxy.port', '8200'),
+    'rpcPort' => env('suframeProxy.rpcPort', '9200'),
     'timeout' => 2.5,
     'apiGetway' => [
         'enable' => false,
@@ -14,8 +14,8 @@ return [
         'port' => '',
     ],
     'registerServer' => [
-        'host' => '127.0.0.1',
-        'port' => 9009
+        'host' => env('suframeProxy.registerServerHost', '127.0.0.1'),
+        'port' => env('suframeProxy.registerServerPort', '9200')
     ],
     'services' => [
         'suframe' => \suframe\think\services\SuframeService::class
