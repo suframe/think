e401a7710a20f8eb796692a149315a01e02549eb
shulinqian <330576744@qq.com>
2020-01-04 16:50:06 +0800

    同步swoole.php的配置

--- src/config/suframeProxy.php
+++ src/config/suframeProxy.php
@@ -5,8 +5,8 @@ return [
     'path' => env('suframeProxy.path', '/demo'),
     'name' => env('suframeProxy.name', 'demo'), //用于接口生成命名空间
     'host' => env('SWOOLE_HOST', '127.0.0.1'),
-    'port' => env('SWOOLE_PORT', '8200'),
-    'rpcPort' => env('SWOOLE_RPC_PORT', '9200'),
+    'port' => env('SWOOLE_PORT', 80),
+    'rpcPort' => env('SWOOLE_RPC_PORT', 9000),
     'timeout' => 2.5,
     'apiGetway' => [
         'enable' => false,
@@ -15,7 +15,7 @@ return [
     ],
     'registerServer' => [
         'host' => env('suframeProxy.registerHost', '127.0.0.1'),
-        'port' => env('suframeProxy.registerPort', '9200')
+        'port' => env('suframeProxy.registerPort', 9000)
     ],
     'services' => [
         'suframe' => \suframe\think\services\SuframeService::class
